4dbda7fdcd6a73870c58be8388f6de97680760e0
Azeem <khanlodhi9897@gmai.com>
2021-01-13 14:05:20 +0530

    add all files

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule } from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents] 
+})
+export class MaterialModule { }
